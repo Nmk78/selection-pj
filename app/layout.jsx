46ad9e1d534bd29c2be29b447555c42814f04fa9
Nmk78 <naymyokhant78@gmail.com>
2023-12-17 19:13:00 +0630

    Need to fine Tune Crown Img and Policy Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,20 +2,7 @@ import "@styles/global.css";
 import Nav from "components/Nav";
 import { ReactQueryProvider } from "util/ReactQueryProvider";
 import { Analytics } from '@vercel/analytics/react';
-// export const metadata = {
-//   title: "UCS(Myeik) | Online Selection Voting System.",
-//   description: "Online Voting System for UCS(Myeik)",
-//   meta: [
-//     {
-//       name: "viewport",
-//       content:
-//         "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
-//     },
-//     <meta property="og:title" content="Title Here" />,
-//     <meta property="og:description" content="Description Here" />,
-//     <meta property="og:image" content="https://i.imgur.com/9rgUcY5.jpg" />,
-//   ],
-// };
+
 
 export const metadata = {
   title: "UCS(Myeik) | Online Selection Voting System.",
