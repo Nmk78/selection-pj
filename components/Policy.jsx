e1f10e24f20c9ddc96ea2c692cbb6316d03c73e5
Nmk78 <naymyokhant78@gmail.com>
2023-12-20 19:02:52 +0630

    Edited Policy

--- components/Policy.jsx
+++ components/Policy.jsx
@@ -18,8 +18,8 @@ const Policy = () => {
           Introduction
         </p>
         <p className="c text-center">
-          The voting policy was made by the organizers and senior students.
-          The objective is to ensure a fair, transparent, and inclusive voting
+          The voting policy was made by the organizers and senior students. The
+          objective is to ensure a fair, transparent, and inclusive voting
           experience, promoting student engagement and representation.
         </p>
 
@@ -31,34 +31,38 @@ const Policy = () => {
           (Myeik) are eligible to vote with pre-distributed secret keys.{" "}
           <span className="text-red-500 text-center">
             One secret key will only be eligible for one male candidate and one
-            female candidate.
+            female candidate in each round (It was total 2 rounds).
           </span>
         </p>
 
         <p className="c text-center">
           For public voters, we provide a place to obtain a secret key. One
-          person can only get one secret key.
+          person can only get one secret key.(Public voter can only vote in
+          first round.)
         </p>
 
         <p className="font-semibold text-xl italic c text-center mt-4 mb-2">
           Vote Count and Result Conclusion
         </p>
         <p className="c text-center">
-          We will count votes from students and public voters in the first round.
-          After the first round, the top 10 male and female candidates will
-          participate in a questions and answers section. Following that,
-          teachers will cast their votes. The candidate with the highest votes
-          will be crowned as the King, and the second-highest voted candidate
-          will be the Prince. Similarly, for female candidates, the one with the
-          highest votes will become the Queen, and the second-highest voted
-          candidate will be the Princess.
+          We will count votes from students and public voters in the first
+          round. After the first round, the top 5 male and female candidates
+          will participate in a questions and answers section. Following that,
+          Second round will open and student and teacher can vote for second
+          round and some teachers and trainers will cast their votes. For
+          casters, Each caster will rate the candidates from 1 to 5 each rate
+          equavilent to 1 vote. The candidate with the highest votes will be
+          crowned as the King, and the second-highest voted candidate will be
+          the Prince. Similarly, for female candidates, the one with the highest
+          votes will become the Queen, and the second-highest voted candidate
+          will be the Princess.
         </p>
 
         <p className="font-semibold text-xl italic c text-center mt-4 mb-2">
           Results Announcement
         </p>
         <p className="c text-center">
-          Results will be live shortly after the presenter announces them on the
+          Results will be live after the presenter announces them on the
           stage. You can return to the home page to view the results.
         </p>
       </div>
